daae626fa4959e3a5fc70b2587519f9dc7bbc710
mbatoul <batoulmathis@gmail.com>
2020-04-16 23:47:03 +0200

    Add Selection Sort

---
+++ plugins/selection-sort.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import { timer, delayInMs } from '~/plugins/utils';
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $selectionSort(): void
+  }
+}
+
+Vue.prototype.$selectionSort = async function () {
+  let newArray: number[] = this.$store.state.arrayOfBars;
+  let startIdx = 0;
+  while (startIdx < newArray.length - 1) {
+    let smallestIdx = startIdx;
+    this.$store.dispatch('setPointerOne', smallestIdx);
+    for (let i = startIdx + 1; i < newArray.length; i++) {
+      this.$store.dispatch('setPointerTwo', i);
+      if (newArray[smallestIdx] > newArray[i]) {
+        smallestIdx = i;
+      }
+      this.$store.dispatch('replaceArrayOfBars', []);
+      this.$store.dispatch('replaceArrayOfBars', newArray);
+      await timer(delayInMs(this.$store.state.numberOfBars));
+    }
+    const tmp = newArray[smallestIdx];
+    newArray[smallestIdx] = newArray[startIdx];
+    newArray[startIdx] = tmp;
+    startIdx++;
+  }
+}
