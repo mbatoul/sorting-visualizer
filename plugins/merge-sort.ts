2cacbbaae45ca6549810f3e194c2c99205e75858
mbatoul <batoulmathis@gmail.com>
2020-09-03 19:02:40 +0200

    Implemented Merge Sort algorithm (O(N*logN) space version)

--- plugins/merge-sort.ts
+++ plugins/merge-sort.ts
@@ -9,9 +9,42 @@ declare module 'vue/types/vue' {
 
 Vue.prototype.$mergeSort = async function () {
   let newArray: number[] = this.$store.state.arrayOfBars;
-  this.$store.dispatch('setPointerOne', null);
-  this.$store.dispatch('setPointerTwo', null);
-  this.$store.dispatch('replaceArrayOfBars', []);
-  await this.$store.dispatch('replaceArrayOfBars', newArray);
-  await timer(delayInMs(numberOfBars));
+  console.log(typeof this.$store)
+  mergeSort(newArray);
+}
+
+async function mergeSort(array: number[]) {
+  if (array.length < 2) return;
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle + 1, array.length - 1);
+  mergeSort(left);
+  mergeSort(right);
+  merge(array, left, right);
+}
+
+async function merge(array: number[], left: number[], right: number[]) {
+  let i = 0
+  let j = 0
+  let k = 0
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      array[k] = left[i]
+      i += 1
+    } else {
+      array[k] = right[j]
+      j += 1
+    }
+    k += 1
+  }
+  while (i < left.length) {
+    array[k] = left[i]
+    k += 1
+    i += 1
+  }
+  while (j < right.length) {
+    array[k] = right[j]
+    k += 1
+    j += 1
+  }
 }
